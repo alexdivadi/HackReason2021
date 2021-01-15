86f26a3b81c541a05b43d5cf8995a8dd897c7223
David Allen <mrbarkface513@gmail.com>
2021-01-15 12:48:40 -0600

    Update restaurant_recommendations.pl

--- restaurant_recommendations.pl
+++ restaurant_recommendations.pl
@@ -11,14 +11,14 @@ menu([X|T]).
 american(X) :- not ethnic(X).
 vegan(X) :- -contains_animal_products(X).
 vegetarian(X) :- not contains_animal_products(X).
-gourmet(X). junk(X).
+gourmet(X) :- -regular(X).
+junk(X) :- -healthy(X).
 
 %Restaurant attributes
-cheap(restaurant(X,_,P)) :- restaurant(X,_,P), P < 2.
-fast_food(restaurant(N,M,P)):- cheap(restaurant(N,M,P)) contains(Food, M), junk(Food), not gourmet(Food).
-expensive(restaurant(X,_,P)) :- restaurant(X,_,P), P > 3.
-fancy(X) :- expensive(X).
-fancy(restaurant(Name, Menu, _)) :- contains(Food, Menu), gourmet(Food).
+cheap(restaurant(N,M,P)) :- restaurant(N,M,P), P =< 2.
+fast_food(restaurant(N,M,P)):- cheap(restaurant(N,M,P)), contains(Food, M), junk(Food), not gourmet(Food).
+expensive(restaurant(N,M,P)) :- restaurant(N,M,P), P > 3.
+fancy(restaurant(N,M,P)) :- expensive(restaurant(N,M,P)), contains(Food, M), gourmet(Food).
 
 %Foods
 food(cheeseburger). contains_animal_products(cheeseburger).
@@ -27,16 +27,16 @@ food(fries). junk(fries).
 food(taco). contains_animal_products(taco).
 food(burrito). contains_animal_products(burrito). ethnic(burrito).
 food(pizza). contains_animal_products(pizza).
-food(salad). vegan(salad).
+food(salad). vegan(salad). healthy(salad).
 food(curry). ethnic(curry).
 food(sushi). contains_animal_products(sushi). ethnic(sushi). gourmet(sushi).
 
 %Restaurants
-restaurant("The Spaghetti Factory", [pizza, pasta], 4).
-restaurant("Bob's Bistro", [cheeseburger, salad, fries], 3).
-restaurant("Torchy's",[taco, burrito, fries], 1).
-restaurant("Taj Mahal",[curry, salad], 2).
-restaurant("Sushi House",[sushi], 5).
+restaurant('The Spaghetti Factory', [pizza, pasta], 4).
+restaurant('Bob\'s Bistro', [cheeseburger, salad, fries], 3).
+restaurant('Torchy\'s',[taco, burrito, fries], 1).
+restaurant('Taj Mahal',[curry, salad], 2).
+restaurant('Sushi House',[sushi], 5).
 
 %Queries
 %?- fancy(_).
